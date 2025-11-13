968712f003336b60325e4e7566eb84c5e50759e0
MartinK-27 <48522161@est.ort.edu.ar>
2025-11-13 16:20:59 -0300

    no se

---
+++ base_de_datos/README.md
@@ -0,0 +1,3 @@
+# base_de_datos
+
+A new Flutter project.
---
+++ base_de_datos/android/app/src/main/kotlin/com/example/base_de_datos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.base_de_datos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
